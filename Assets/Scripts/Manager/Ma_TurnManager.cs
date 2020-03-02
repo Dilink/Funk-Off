54445b4783da7cf1ff76229d82f1a9fa8bcd1ced
Nils Vandamme <nils.vandamme98@gmail.com>
2020-03-02 18:47:05 +0100

    hamdulillah

--- Assets/Scripts/Manager/Ma_TurnManager.cs
+++ Assets/Scripts/Manager/Ma_TurnManager.cs
@@ -5,14 +5,42 @@ using System;
 
 public class Ma_TurnManager : MonoBehaviour
 {
+    public static Ma_TurnManager instance; // Static instance
 
-    void BeginTurn()
+    [Header("Turns stats")]
+    public int MaxTurn; // Max number of turns for this level
+    public int CurrentTurn; // Current turn number
+
+    [Header("Moves stats")]
+    public int MaxMoves; // Max moves per character and per turn
+
+    [Header("Debug")]
+    //[HideInInspector]
+    public List<Mb_PlayerController> PlayersList; // A list of all the player characters
+
+    public void BeginTurn()
     {
-        throw new NotImplementedException();
+        // Reset all player characters move number
+        for(int i =0; i < PlayersList.Count; i++)
+        {
+            // PlayersList[i].moves? = MaxMoves;
+        }
+
+        Debug.LogError("Turn " + CurrentTurn + " has begun");
     }
 
-    void EndTurn()
+    public void EndTurn()
     {
-        throw new NotImplementedException();
+        // Pass to the next turn
+        if(CurrentTurn <= MaxTurn)
+        {
+            CurrentTurn++;
+        }
+        else // End the level
+        {
+            // End the level
+        }
+
+        Debug.LogError("Turn " + CurrentTurn + " has ended");
     }
 }
