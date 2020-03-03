b0be221db551920841488def682b827fd51a1559
Dilink <dilink4@gmail.com>
2020-03-03 10:53:24 +0100

    Add Pattern validation

--- Assets/Scripts/Manager/Ma_PatternManager.cs
+++ Assets/Scripts/Manager/Ma_PatternManager.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 using System;
 
 public class Ma_PatternManager : MonoBehaviour
 {
+    public OrderedDictionary currentPatternsList = new OrderedDictionary();
+
     void CheckGridForPattern()
     {
         throw new NotImplementedException();
     }
 
-    void PatternValidation(Sc_Pattern pattern)
+    bool PatternValidation(Sc_Pattern pattern)
     {
-        throw new NotImplementedException();
+        return currentPatternsList.Contains(pattern.matrix);
     }
     
     void GenerateStartPattern()
