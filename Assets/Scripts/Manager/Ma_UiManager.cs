b09b89dfc9f045757460ae747600ed1cde9a69c8
Nils Vandamme <nils.vandamme98@gmail.com>
2020-03-03 10:29:38 +0100

    UI Canvas prefab + Funkbar fill method (untested)

--- Assets/Scripts/Manager/Ma_UiManager.cs
+++ Assets/Scripts/Manager/Ma_UiManager.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Ma_UiManager : MonoBehaviour
 {
     [SerializeField] Mb_PlayerCard[] allPlayerUi;
 
+    [Header("Turnsbar elements")]
+    public TMP_Text TurnsbarText;
+
+    [Header("Funkbar elements")]
+    public Image FunkbarFillImg;
+    public RectTransform FunkbarFillRect;
+    public Image FunkbarCursorImg;
+    public RectTransform FunkbarCursorRect;
+
+    // Change the visual of the Funkbar to the indicated percentage
     public void UpdateFunkBar(float funkPercentage)
     {
-
+        FunkbarFillImg.fillAmount = funkPercentage / 100;
+        FunkbarCursorRect.anchoredPosition = new Vector2(FunkbarFillRect.sizeDelta.x * FunkbarFillImg.fillAmount, FunkbarCursorRect.anchoredPosition.y);
     }
 
     public void UpdateCharacterUi(Mb_PlayerController playerConcerned, int MoveLeft, int MaxMove)
