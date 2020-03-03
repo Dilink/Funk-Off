77d879d9c42dc78474b3b6230680dda0402a9311
Nils Vandamme <nils.vandamme98@gmail.com>
2020-03-03 10:53:20 +0100

    Ui canvas + Manager updated

--- Assets/Scripts/Manager/Ma_UiManager.cs
+++ Assets/Scripts/Manager/Ma_UiManager.cs
@@ -6,6 +6,8 @@ using TMPro;
 
 public class Ma_UiManager : MonoBehaviour
 {
+    public static Ma_UiManager instance;
+
     [SerializeField] Mb_PlayerCard[] allPlayerUi;
 
     [Header("Turnsbar elements")]
@@ -16,6 +18,24 @@ public class Ma_UiManager : MonoBehaviour
     public RectTransform FunkbarFillRect;
     public Image FunkbarCursorImg;
     public RectTransform FunkbarCursorRect;
+
+    private void Reset()
+    {
+        // Turnsbar elements
+        TurnsbarText = GameObject.Find("TurnsBar_TextTurnsCount").GetComponent<TMP_Text>();
+
+        // Funkbar elements
+        FunkbarFillImg = GameObject.Find("Funkbar_Fill").GetComponent<Image>();
+        FunkbarFillRect = GameObject.Find("Funkbar_Fill").GetComponent<RectTransform>();
+        FunkbarCursorImg = GameObject.Find("Funkbar_Cursor").GetComponent<Image>();
+        FunkbarCursorRect = GameObject.Find("Funkbar_Cursor").GetComponent<RectTransform>();
+    }
+
+    // Update the text of the Turnsbar to display current turn / Max turns
+    public void UpdateTurnsbarText()
+    {
+        TurnsbarText.text = Ma_TurnManager.instance.CurrentTurn.ToString() + "/" + Ma_TurnManager.instance.MaxTurn.ToString();
+    }
 
     // Change the visual of the Funkbar to the indicated percentage
     public void UpdateFunkBar(float funkPercentage)
