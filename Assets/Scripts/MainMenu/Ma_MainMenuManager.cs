27f9ab13a56bbd3e1b6c5a9ee4d1275e56cba181
Nils <nils.vandamme98@gmail.com>
2020-03-08 18:12:26 +0100

    Base menu navigation

--- Assets/Scripts/MainMenu/Ma_MainMenuManager.cs
+++ Assets/Scripts/MainMenu/Ma_MainMenuManager.cs
@@ -11,11 +11,154 @@ public class Ma_MainMenuManager : Singleton<Ma_MainMenuManager>
     private GameObject[] selectedEmplacementsGameobjects = new GameObject[3];
     private bool[] selectedEmplacementsStatus = new bool[3];
 
+    [Header("Navigation elements")]
+
+    public RectTransform currentScreen;
+    [Space]
+    public RectTransform MainMenuRect;
+    public RectTransform OptionsRect;
+    public RectTransform LevelRect;
+    public RectTransform CharacterRect;
+    public RectTransform ItemsUpgradeRect;
+
     [Header("Team Builder elements")]
     public GridLayoutGroup cardsGrid;
     public RectTransform selectedCharactersEmplacementsRect;
 
+    private void Start()
+    {
+        currentScreen = MainMenuRect;
+    }
 
+    // -------------
+    // NAVIGATION
+    // -------------
+
+    // MAIN MENU
+    public void DisplayMainMenuScreen(bool fromLeft)
+    {
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0.6f, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0.6f, false);
+
+        }
+
+        currentScreen = MainMenuRect;
+
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0, false);
+        }
+
+        currentScreen.DOAnchorPos3DX(0, 0.6f, false);
+    }
+
+
+    // OPTIONS
+    public void DisplayOptionsScreen(bool fromLeft)
+    {
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0.6f, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0.6f, false);
+        }
+
+        currentScreen = OptionsRect;
+
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0, false);
+        }
+
+        currentScreen.DOAnchorPos3DX(0, 0.6f, false);
+    }
+
+
+    // LEVEL
+    public void DisplayLevelScreen(bool fromLeft)
+    {
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0.6f, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0.6f, false);
+        }
+
+        currentScreen = LevelRect;
+
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0, false);
+        }
+
+        currentScreen.DOAnchorPos3DX(0, 0.6f, false);
+    }
+
+
+    // CHARACTERS
+    public void DisplayCharacterScreen(bool fromLeft)
+    {
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0.6f, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0.6f, false);
+
+        }
+
+        currentScreen = CharacterRect;
+
+        if (fromLeft)
+        {
+            currentScreen.DOAnchorPos3DX(-2000, 0, false);
+        }
+        else
+        {
+            currentScreen.DOAnchorPos3DX(2000, 0, false);
+
+        }
+
+        currentScreen.DOAnchorPos3DX(0, 0.6f, false);
+    }
+
+    public void DisplayItemsUpgradeScreen()
+    {
+        ItemsUpgradeRect.DOAnchorPos3DY(0, 0.6f, false);
+    }
+
+    public void HideItemsDisplayScreen()
+    {
+        ItemsUpgradeRect.DOAnchorPos3DY(1200, 0.6f, false);
+    }
+
+    // -------------
+    // TEAM BUILDER
+    // -------------
+
+    // Add the card to the selected characters zone
     public void AddCardToSelectedEmplacements(GameObject card)
     {
         for(int i = 0; i < selectedEmplacementsStatus.Length; i++)
@@ -30,6 +173,7 @@ public class Ma_MainMenuManager : Singleton<Ma_MainMenuManager>
         }
     }
 
+    // Remove the card to the selected characters zone
     public void RemoveCardFromSelectedEmplacement(GameObject card)
     {
         for(int j = 0; j < selectedEmplacementsGameobjects.Length; j++)
@@ -43,6 +187,7 @@ public class Ma_MainMenuManager : Singleton<Ma_MainMenuManager>
         }
     }
 
+    // Return all the scriptables objects of the cards in selected characters zone
     public void GetSelectedPlayerScriptablesList()
     {
         for(int k = 0; k < selectedEmplacementsGameobjects.Length; k++)
--- Assets/Scripts/MainMenu/TeamBuilder/Sc_TeamBuilderPlayerCards.cs
+++ Assets/Scripts/MainMenu/TeamBuilder/Sc_TeamBuilderPlayerCards.cs
@@ -7,11 +7,14 @@ using DG.Tweening;
 public class Sc_TeamBuilderPlayerCards : MonoBehaviour, IDragHandler, IDropHandler, IBeginDragHandler
 {
     public Sc_CharacterParameters characterParameters;
-    private Vector3 originalPosition;
+    public Vector3 originalPosition;
 
-    private void Awake()
+    private bool firstDrag;
+
+    private void Start()
     {
         SetOriginalPosition();
+        //Debug.Log("og position set");
     }
 
     public void SetOriginalPosition()
@@ -21,6 +24,12 @@ public class Sc_TeamBuilderPlayerCards : MonoBehaviour, IDragHandler, IDropHandl
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (!firstDrag)
+        {
+            SetOriginalPosition();
+            firstDrag = true;
+        }
+
         //Ma_MainMenuManager.Instance.cardsGrid.enabled = false;
         transform.SetAsLastSibling();
     }
@@ -34,7 +43,7 @@ public class Sc_TeamBuilderPlayerCards : MonoBehaviour, IDragHandler, IDropHandl
     {
         if(RectTransformUtility.RectangleContainsScreenPoint(Ma_MainMenuManager.Instance.selectedCharactersEmplacementsRect, new Vector2(Input.mousePosition.x, Input.mousePosition.y)))
         {
-            Debug.Log("TBPC rect contains");
+            //Debug.Log("TBPC rect contains");
             Ma_MainMenuManager.Instance.RemoveCardFromSelectedEmplacement(this.gameObject);
             Ma_MainMenuManager.Instance.AddCardToSelectedEmplacements(this.gameObject);
         }
