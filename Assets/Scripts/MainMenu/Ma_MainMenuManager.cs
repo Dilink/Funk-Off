b3786d425395ba03395b33a41638b3b1d2f3dd63
Antonin Druelle <a.druelle@gmail.com>
2020-03-09 15:04:25 +0100

    Menu Selection level

--- Assets/Scripts/MainMenu/Ma_MainMenuManager.cs
+++ Assets/Scripts/MainMenu/Ma_MainMenuManager.cs
@@ -30,6 +30,16 @@ public class Ma_MainMenuManager : MonoBehaviour
 
     [Header("Levels elements")]
 
+    public GameObject[] levelPlayButtonsGameobjects = new GameObject[3];
+    public GameObject[] level1GradesGameobjects = new GameObject[5]; // image du grade
+    public GameObject[] level2GradesGameobjects = new GameObject[5];
+    public GameObject[] level3GradesGameobjects = new GameObject[5];
+    public int currentLevel = 1;
+    public int levelDone;
+    public int gradeLevelDone;
+
+
+
     [Header("Items elements")]
 
     [Header("Team Builder elements")]
@@ -187,6 +197,78 @@ public class Ma_MainMenuManager : MonoBehaviour
     // -------------
     #region levels
 
+    public void CheckLevelValidation(int levelClick)
+    {
+        //Check si on a débloqué le niveau que lequel on clique et si l'on a pas déja cliqué dessus
+        if(currentLevel >= levelClick && levelPlayButtonsGameobjects[levelClick].activeInHierarchy == false)
+        {
+            for (int i = 0; i < levelPlayButtonsGameobjects.Length; i++)
+            {
+                if (levelPlayButtonsGameobjects[i].activeInHierarchy)
+                {
+                    levelPlayButtonsGameobjects[i].SetActive(false);
+                    
+                }
+            }
+
+            levelPlayButtonsGameobjects[levelClick].SetActive(true);
+            
+        }
+    }
+    public void LevelDoneBtn()
+    {
+        GradeLevel(levelDone, gradeLevelDone);
+    }
+    public void GradeLevel (int level, int grade)
+    {
+
+        if (level == 2)
+        {
+            for (int i = 0; i <= grade; i++)
+            {
+                if (level3GradesGameobjects[i].activeInHierarchy)
+                {
+                    level3GradesGameobjects[i].SetActive(false);
+                    
+                }
+
+                level3GradesGameobjects[grade].SetActive(true);
+
+            }
+
+            
+        }
+        if (level == 1)
+        {
+
+            for (int i = 0; i <= grade; i++)
+            {
+                if (level2GradesGameobjects[i].activeInHierarchy)
+                {
+                    level2GradesGameobjects[i].SetActive(false);
+                    
+                }
+
+                level2GradesGameobjects[grade].SetActive(true);
+            }
+            
+        }
+
+        if (level == 0)
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+               
+                if (level1GradesGameobjects[i].activeInHierarchy)
+                {
+                    level1GradesGameobjects[i].SetActive(false);
+                    
+                }
+                    level1GradesGameobjects[grade].SetActive(true);
+            }
+            
+        }
+    }
     #endregion
     // -------------
     // ITEMS UPGRADE
