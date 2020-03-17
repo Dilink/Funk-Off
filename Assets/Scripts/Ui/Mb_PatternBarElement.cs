1b662d74542215f1b7a4af693beb0d88217ad093
pier <p.Soriot@rubika-edu.com>
2020-03-17 19:45:01 +0100

    bricolj prefab pattern element

--- Assets/Scripts/Ui/Mb_PatternBarElement.cs
+++ Assets/Scripts/Ui/Mb_PatternBarElement.cs
@@ -14,13 +14,17 @@ public class Mb_PatternBarElement : MonoBehaviour
     [Header("ComboPart")]
     public ParticleSystem[] FX;
     public Image backGroundColor;
-    public Image multiplierImg;
+    public RectTransform multiplierImg;
     public TMP_Text multiplierText;
+    Vector2 basePosTorwardPatternBar;
 
+    [Header("AnimTweaking")]
+    [SerializeField] Vector2 posToAddOnCompletion;
 
     private void Start()
     {
         rectTransform = gameObject.GetComponent<RectTransform>();
+        basePosTorwardPatternBar = multiplierImg.localPosition;
     }
 
     public void PlayFX(int index)
@@ -42,5 +46,27 @@ public class Mb_PatternBarElement : MonoBehaviour
     {
         multiplierText.text = "";
         backGroundColor.color = GameManager.Instance.comboManager.colorNone;
+    }
+
+    void DelockBonusIcon()
+    {
+        multiplierImg.SetParent(null);
+    }
+
+    void LockBonusIcon()
+    {
+        multiplierImg.SetParent(transform);
+    }
+
+    public void AnimBonusCompleted()
+    {
+        float y = posToAddOnCompletion.y - 10;
+        multiplierImg.DOLocalMoveY((basePosTorwardPatternBar.y +  y), .8f).OnComplete(()=>{
+            multiplierImg.DOLocalMoveY((10), .3f);
+            multiplierImg.DOScale(1.2f, .3f).OnComplete(() => {
+                multiplierImg.DOScale(1, 1.1f);
+                multiplierImg.DOLocalMoveY(-posToAddOnCompletion.y, 1.1f);
+            });
+        });
     }
 }
